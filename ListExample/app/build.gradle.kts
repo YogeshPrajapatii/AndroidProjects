b84cdd6f16bb90bba8851ab4c9dee1ed062ce2de
Yogesh Prajapati <verma981133@gmail.com>
2025-01-29 21:17:14 +0530

    Added new feature

--- ListExample/app/build.gradle.kts
+++ ListExample/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.devleoperyogi.listexample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.devleoperyogi.listexample"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
